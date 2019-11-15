f29c6e8f1edc509688b0193ae277b99962aa99e5
Tomas <tomas.silvestre9@gmail.com>
2019-11-15 20:31:20 -0300

    login y signup modificados

--- forms.py
+++ forms.py
@@ -1,4 +1,3 @@
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired, Email, Length
@@ -8,23 +7,23 @@ class SignupForm(FlaskForm):
     name = StringField('Nombre', validators=[DataRequired(), Length(max=64)])
     password = PasswordField('Password', validators=[DataRequired()]) #se podria agregar lo de repetir la contraseña
     email = StringField('Email', validators=[DataRequired(), Email()])
-    submit = SubmitField('Registrar')
+    submit = SubmitField('Sign up')
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()]) 
     remember_me = BooleanField('Recuérdame')
-    submit = SubmitField('Login')
+    submit = SubmitField('Log in')
 
 class ContractForm(FlaskForm): #creo formulario de contrato
-    title = StringField('Título', validators=[DataRequired(), Length(max=128)])
+    title = StringField('Title', validators=[DataRequired(), Length(max=128)])
     description = TextAreaField('Description')
-    price = StringField('Precio', validators=[DataRequired()])
-    submit = SubmitField('Crear contrato')
+    price = StringField('Price', validators=[DataRequired()])
+    submit = SubmitField('Create contract')
 
 
 class WalletForm(FlaskForm): #formulario de billetera
-    name = StringField('Título', validators=[DataRequired(), Length(max=128)])
-    content = TextAreaField('Contenido', validators =[DataRequired()])
-    submit = SubmitField('Conectar billetera')
+    name = StringField('Title', validators=[DataRequired(), Length(max=128)])
+    content = TextAreaField('Content', validators =[DataRequired()])
+    submit = SubmitField('Connect wallet')
--- run.py
+++ run.py
@@ -89,7 +89,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for('index'))
 
 
 #formulario para crear los contratos, tambien muestra los existentes
